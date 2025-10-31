121cdc25d2fd617473514d84a88065ff271b0a52
Özkan Parlar <129742336+ozkanparlar@users.noreply.github.com>
2025-10-31 20:45:46 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-31T17:45:35.489Z
-Commit: Add export functionality for reports
+Timestamp: 2025-10-31T17:45:45.193Z
+Commit: Fix memory leak in event handlers
