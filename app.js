c9619d8bf22126884e857c7d5417c140e266f07c
Özkan Parlar <129742336+ozkanparlar@users.noreply.github.com>
2025-10-31 20:45:51 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-31T17:45:40.750Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-10-31T17:45:50.836Z
+Commit: Fix CSS styling bug on mobile devices
