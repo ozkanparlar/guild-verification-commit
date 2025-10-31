5a0d7df6424685dc68c0b11a867c1b9578411a7a
Özkan Parlar <129742336+ozkanparlar@users.noreply.github.com>
2025-10-31 20:54:30 +0300

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-31T17:54:22.225Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-10-31T17:54:29.151Z
+Commit: Patch API endpoint error handling
