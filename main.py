a58a9f91fdb3298e5dc05b1b7f4298faa91dc6f5
Özkan Parlar <129742336+ozkanparlar@users.noreply.github.com>
2025-10-31 20:44:46 +0300

    Resolve timezone issues in date handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-31T17:44:33.994Z
-Commit: Implement user profile customization
+Timestamp: 2025-10-31T17:44:45.549Z
+Commit: Resolve timezone issues in date handling
