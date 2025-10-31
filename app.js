720ad2e9517f101dec625e59dc1c6369104c8030
Özkan Parlar <129742336+ozkanparlar@users.noreply.github.com>
2025-10-31 20:54:19 +0300

    Fix memory leak in event handlers

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-31T17:54:17.098Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-10-31T17:54:18.767Z
+Commit: Fix memory leak in event handlers
