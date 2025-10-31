c36cfa3c11d2c261ca9939813122c43d5e54c272
Özkan Parlar <129742336+ozkanparlar@users.noreply.github.com>
2025-10-31 20:54:43 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-31T17:54:37.607Z
-Commit: Implement user profile customization
+Timestamp: 2025-10-31T17:54:42.637Z
+Commit: Fix broken links in documentation
