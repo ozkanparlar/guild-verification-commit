7e29a1f8c78dcd34f74b971e75097d11bcd10f18
Özkan Parlar <129742336+ozkanparlar@users.noreply.github.com>
2025-10-31 20:46:00 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-31T17:45:14.747Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-10-31T17:45:59.345Z
+Commit: Correct typo in configuration file
