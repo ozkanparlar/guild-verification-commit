75fa9dee5f5b8496b47b081ed87829a6f0c99124
Özkan Parlar <129742336+ozkanparlar@users.noreply.github.com>
2025-10-31 20:45:41 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-31T17:44:47.713Z
-Commit: Add export functionality for reports
+Timestamp: 2025-10-31T17:45:40.750Z
+Commit: Fix broken links in documentation
